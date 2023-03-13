90dae104bba93d99223ece58e750a8c97fb494ad
swaraj bachu <rajswaraj.r@gmail.com>
2023-03-14 02:55:52 +0530

    mailchimp code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends React.Component {
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <script
+            id="mcjs"
+            src="https://chimpstatic.com/mcjs-connected/js/users/11745289bae4b1acf862f6450/c2ad266f7d39b4c9568a4951f.js"
+            async
+            defer
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
